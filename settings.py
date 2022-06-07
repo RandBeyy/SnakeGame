00724d6844950df693e3d7b7a62b307a30fe552c
Rostislav <102387447+RandBeyy@users.noreply.github.com>
2022-06-07 14:15:35 +0300

    Base game area

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+
+    def __init__(self):
+        #Screen settings
+        self.screen_size = (800, 800)
+        self.bg_color = (240, 240, 240)
+        
+
+        #Cell settings
+        self.cell_size = (50,50)
+        self.gray_cell_color = (200, 200, 200)
+        self.white_cell_color = (240, 240, 240)
---
+++ snake_game.py
@@ -0,0 +1,58 @@
+import sys
+import pygame
+from settings import Settings
+
+class SnakeGame:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.game_area = []
+
+        self.screen = pygame.display.set_mode(self.settings.screen_size)
+
+
+    def run_game(self):
+        self._generate_game_area()
+        self._set_screen()
+        while True:
+            self._update_screen()
+            self._check_events()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+
+    def _update_screen(self):
+        pygame.display.flip()
+
+
+    def _generate_game_area(self):
+        color = True
+        self.game_area = [[pygame.Rect(x,y,50,50) for x in range(0,801)[::50]] for y in range(0,801)[::50]]
+
+    def _set_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        color = True
+        for row in self.game_area:
+            for cell in row:
+                if (color):
+                    color = False
+                    pygame.draw.rect(self.screen, self.settings.gray_cell_color, cell)
+                else:
+                    color = True
+                    pygame.draw.rect(self.screen, self.settings.white_cell_color, cell)
+                
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    sg = SnakeGame()
+    sg.run_game()
